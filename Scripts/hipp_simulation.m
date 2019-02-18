b69a2638b3d8984c37b660a381ab9465cba872ab
StefanoBuccelli <stefano.buccelli@gmail.com>
2019-02-18 12:49:08 +0100

    improving plots

--- Scripts/hipp_simulation.m
+++ Scripts/hipp_simulation.m
@@ -1,21 +1,20 @@
 clear
 clc
 close all
-%% Trying to replicate supplementary figures from hipp (simulation) 
+%% Trying to replicate supplementary figures from hipp 2012 (simulation)
 
 signal_len=1e4; % samples
 
-for B=.2 % Rayleigh parameter (equal to the std of the two original gauss distributions)
+B = 2; % Rayleigh parameter (equal to the std of the two original gauss distributions)
 %%
 % x = raylrnd(B,1,signal_len) +1i*raylrnd(B,1,signal_len);
 % x_2 = raylrnd(B+1,1,signal_len) +1i*raylrnd(B+1,1,signal_len);
 rng(4)
-ampiezza=raylrnd(B,1,signal_len);
-rng(4)
-p = raylcdf(ampiezza,B);
-ampiezza_remapped=1-p;
-rng(4)
-ampiezza_back_trasf=raylinv(ampiezza_remapped,B);
+amplitude=raylrnd(B,1,signal_len);
+p = raylcdf(amplitude,B);
+amplitude_remapped=1-p;
+amplitude_back_transf=raylinv(amplitude_remapped,B);
+
 phases_random=rand(1,signal_len).*2*pi;
 phases_random=phases_random-mean(phases_random);
 phases_random_2=rand(1,signal_len).*2*pi;
@@ -23,46 +22,69 @@ phases_random_2=phases_random_2-mean(phases_random_2);
 phases_random_3=rand(1,signal_len).*2*pi;
 phases_random_3=phases_random_3-mean(phases_random_3);
 
-x = ampiezza.*exp(1i.*phases_random);
-x_2 = ampiezza_back_trasf.*exp(1i.*phases_random_2);
+x = amplitude.*exp(1i.*phases_random);
+x_2 = amplitude_back_transf.*exp(1i.*phases_random_2);
 x_3=raylrnd(B,1,signal_len).*exp(1i.*phases_random_3);
 % x = raylrnd(B,1,signal_len).*exp(1i.*raylrnd(B,1,signal_len));
 % x_2 = raylrnd(B,1,signal_len).*exp(1i.*raylrnd(B,1,signal_len));
 
 %% histogram of x and x_2 amplitudes and phases
 figure
-h_x(1)=subplot(2,3,1);
-histogram(abs(x))
-title('x amplitude')
-h_x(1)=subplot(2,3,2);
-histogram(angle(x))
-title('x phase')
-h_x(1)=subplot(2,3,3);
+h_x(1)=subplot(3,4,1);
 histogram(real(x))
-title('x real')
-h_x(1)=subplot(2,3,4);
-histogram(abs(x_2))
-title('x2 amplitude')
-h_x(1)=subplot(2,3,5);
-histogram(angle(x_2))
-title('x2 phase')
-h_x(1)=subplot(2,3,6);
+title('real(x)','interpreter','none')
+h_x(2)=subplot(3,4,2);
+histogram(imag(x))
+title('imag(x)','interpreter','none')
+h_x(3)=subplot(3,4,3);
+histogram(abs(x))
+title('abs(x)','interpreter','none')
+h_x(4)=subplot(3,4,4);
+histogram(angle(x)*180/pi)
+title('phase(x)','interpreter','none')
+
+h_x(1)=subplot(3,4,5);
 histogram(real(x_2))
-title('x2 real')
+title('real(x_2)','interpreter','none')
+h_x(2)=subplot(3,4,6);
+histogram(imag(x_2))
+title('imag(x_2)')
+h_x(3)=subplot(3,4,7);
+histogram(abs(x_2))
+title('abs(x_2)','interpreter','none')
+h_x(4)=subplot(3,4,8);
+histogram(angle(x_2)*180/pi)
+title('phase(x_2)','interpreter','none')
+
+h_x(1)=subplot(3,4,9);
+histogram(real(x_3))
+title('real(x_3)','interpreter','none')
+h_x(2)=subplot(3,4,10);
+histogram(imag(x_3))
+title('imag(x_3)','interpreter','none')
+h_x(3)=subplot(3,4,11);
+histogram(abs(x_3))
+title('abs(x_3)','interpreter','none')
+h_x(4)=subplot(3,4,12);
+histogram(angle(x_3)*180/pi)
+title('phase(x_3)','interpreter','none')
+
+linkaxes(h_x,'y')
 %%
-range_coherence=-1:.1:1;
+range_coherence=-1:.01:1;
 
 %% initialize variables
 y=zeros(length(range_coherence),signal_len);
 x_power=zeros(length(range_coherence),signal_len);
 x_2_power=zeros(length(range_coherence),signal_len);
+x_3_power=zeros(length(range_coherence),signal_len);
 y_power=zeros(length(range_coherence),signal_len);
 y_ort_x=zeros(length(range_coherence),signal_len);
 x_ort_y=zeros(length(range_coherence),signal_len);
 y_ort_x_power=zeros(length(range_coherence),signal_len);
 x_ort_y_power=zeros(length(range_coherence),signal_len);
 
-rho_all_plain=zeros(length(range_coherence),1);
+rho_all_x_y_plain=zeros(length(range_coherence),1);
 rho_all_ort=zeros(length(range_coherence),1);
 rho_all_x_x_ort_y=zeros(length(range_coherence),1);
 rho_all_x_y_ort_x=zeros(length(range_coherence),1);
@@ -86,32 +108,49 @@ for curr_c=range_coherence
     % power
     x_power(curr_step,:)=abs(x).^2;
     x_2_power(curr_step,:)=abs(x_2).^2;
+    x_3_power(curr_step,:)=abs(x_3).^2;
     y_power(curr_step,:)=abs(y(curr_step,:)).^2;
     
     x_power(curr_step,:)=log10(x_power(curr_step,:));
     x_2_power(curr_step,:)=log10(x_2_power(curr_step,:));
+    x_3_power(curr_step,:)=log10(x_3_power(curr_step,:));
     y_power(curr_step,:)=log10(y_power(curr_step,:));
+    
+    %% figure to compare power signals
+%     figure
+%     plot(x_power(curr_step,:))
+%     hold on
+%     plot(x_2_power(curr_step,:))
+%     plot(x_3_power(curr_step,:))
+%     legend({'x','x_2','x_3'},'interpreter','none')
+%     title('comparing power envelopes')
+%     xlabel('samples')
+%     ylabel('log10( |signal|^2 )')    
+    
     %% plain
-    rho_plain=corrcoef(x_power(curr_step,:),y_power(curr_step,:));
-    rho_all_plain(curr_step)=rho_plain(1,2);
-
-    %% orthogonalization x-y 
-    y_ort_x(curr_step,:)=y(curr_step,:)-real(x.*conj(y(curr_step,:))./(abs(x).^2)).*x; %from hipp
-    x_ort_y(curr_step,:)=x-real(y(curr_step,:).*conj(x)./(abs(y(curr_step,:)).^2)).*y(curr_step,:); %from hipp
+    rho_all_x_y_plain(curr_step)=corr(x_power(curr_step,:)',y_power(curr_step,:)');
+    rho_all_x_x2_plain(curr_step)=corr(x_power(curr_step,:)',x_2_power(curr_step,:)');
+    rho_all_x_x3_plain(curr_step)=corr(x_power(curr_step,:)',x_3_power(curr_step,:)');
+    rho_all_x2_x3_plain(curr_step)=corr(x_2_power(curr_step,:)',x_3_power(curr_step,:)');
+    disp(['correlation between x-x_2: ' num2str(rho_all_x_x2_plain(curr_step)) ' , x-x_3: ' num2str(rho_all_x_x3_plain(curr_step)) ' , x_2-x_3: ' num2str(rho_all_x2_x3_plain(curr_step))])
+    
+    %% orthogonalization x-y
+    y_ort_x(curr_step,:)=y(curr_step,:)-real(x.*conj(y(curr_step,:))./(abs(x).^2)).*x; %from hipp equivalent when powered : siems=imag(y(curr_step,:).*conj(x)./abs(x));
+    x_ort_y(curr_step,:)=x-real(y(curr_step,:).*conj(x)./(abs(y(curr_step,:)).^2)).*y(curr_step,:); %from hipp equivalent when powered : siems=imag(x.*conj(y(curr_step,:))./abs(y(curr_step,:)));
     
     y_ort_x_power(curr_step,:)=abs(y_ort_x(curr_step,:)).^2;
     x_ort_y_power(curr_step,:)=abs(x_ort_y(curr_step,:)).^2;
-    
+%     
     y_ort_x_power(curr_step,:)=log10(y_ort_x_power(curr_step,:));
     x_ort_y_power(curr_step,:)=log10(x_ort_y_power(curr_step,:));
     
-    %% orthogonalization x2-y 
+    %% orthogonalization x2-y
     y_ort_x2(curr_step,:)=y(curr_step,:)-real(x_2.*conj(y(curr_step,:))./(abs(x_2).^2)).*x_2; %from hipp
     x2_ort_y(curr_step,:)=x_2-real(y(curr_step,:).*conj(x_2)./(abs(y(curr_step,:)).^2)).*y(curr_step,:); %from hipp
     
     y_ort_x2_power(curr_step,:)=abs(y_ort_x2(curr_step,:)).^2;
     x2_ort_y_power(curr_step,:)=abs(x2_ort_y(curr_step,:)).^2;
-    
+%     
     y_ort_x2_power(curr_step,:)=log10(y_ort_x2_power(curr_step,:));
     x2_ort_y_power(curr_step,:)=log10(x2_ort_y_power(curr_step,:));
     
@@ -135,24 +174,24 @@ for curr_c=range_coherence
     rho_all_x2_x_ort_y(curr_step)=corr(x_2_power(curr_step,:)',x_ort_y_power(curr_step,:)');
     %% x_2 corr y ortog x
     rho_all_x2_y_ort_x(curr_step)=corr(x_2_power(curr_step,:)',y_ort_x_power(curr_step,:)');
-
+    
     curr_step=curr_step+1;
 end
 
-%% evaluate correlation with the orthogonalized 
-ort_hipp=(atanh(rho_all_x_y_ort_x)./2+atanh(rho_all_y_x_ort_y)./2); %x - y
-ort_hipp_no_atan=((rho_all_x_y_ort_x)./2+(rho_all_y_x_ort_y)./2);
+%% evaluate correlation with the orthogonalized
+ort_hipp_x_y_atanh=(atanh(rho_all_x_y_ort_x)./2+atanh(rho_all_y_x_ort_y)./2); %x - y
+ort_hipp_x_y_no_atanh=((rho_all_x_y_ort_x)./2+(rho_all_y_x_ort_y)./2);
 
-ort_hipp_no_atan_x2=-((rho_all_y_x2_ort_y)./2+(rho_all_x2_y_ort_x2)./2); % x_2 - y
+ort_hipp_x_x2_no_atanh=-((rho_all_y_x2_ort_y)./2+(rho_all_x2_y_ort_x2)./2); % x_2 - y
 
 figure
 subplot(2,1,1)
-plot(range_coherence,ort_hipp)
+plot(range_coherence,ort_hipp_x_y_atanh)
 hold on
-plot(range_coherence,ort_hipp_no_atan)
+plot(range_coherence,ort_hipp_x_y_no_atanh)
 hold on
-plot(range_coherence,rho_all_plain,'k')
-plot(range_coherence,ort_hipp_no_atan_x2,'g')
+plot(range_coherence,rho_all_x_y_plain,'k')
+plot(range_coherence,ort_hipp_x_x2_no_atanh,'g')
 title(['rho (ort hipp) vs rho plain ' num2str(B)])
 legend({'hipp atanh y-x','hipp no atanh y-x','plain','hipp no atanh -(y-x2)'})
 xlabel('coherence')
@@ -160,23 +199,23 @@ ylabel('correlation')
 ylim([-1 1])
 
 subplot(2,1,2)
-plot(range_coherence,ort_hipp_no_atan,'k')
+plot(rho_all_x_y_plain,ort_hipp_x_y_no_atanh,'k')
 title('rho all plain - rho ort hipp')
 hold on
-plot(rho_all_plain,ort_hipp_no_atan_x2,'g')
+plot(rho_all_x_y_plain,ort_hipp_x_x2_no_atanh,'g')
 xlabel('correlation')
 ylabel('corr orth')
 legend({'y - x','-(y -x2)'})
 ylim([-1 1])
 
-m=mean(diff(ort_hipp_no_atan(1:9))./diff(rho_all_plain(1:9)))
-m_2=mean(diff(ort_hipp_no_atan_x2(14:21))./diff(rho_all_plain(14:21))')
-end
+m=mean(diff(ort_hipp_x_y_no_atanh(1:9))./diff(rho_all_x_y_plain(1:9)))
+m_2=mean(diff(ort_hipp_x_x2_no_atanh(14:21))./diff(rho_all_x_y_plain(14:21))')
+
 
 %% figure to compare the different correlations
 figure
 h_s(1)=subplot(2,4,1);
-plot(range_coherence,rho_all_plain,'k.')
+plot(range_coherence,rho_all_x_y_plain,'k.')
 hold on
 plot(range_coherence,rho_all_ort,'m.')
 legend({'plain','(x ort y , y ort x)'},'interpreter','none')
@@ -184,7 +223,7 @@ xlabel('coherence')
 ylabel('correlation coeff')
 
 h_s(2)=subplot(2,4,2);
-plot(range_coherence,rho_all_plain,'k.')
+plot(range_coherence,rho_all_x_y_plain,'k.')
 hold on
 plot(range_coherence,rho_all_x_x_ort_y,'bo')
 plot(range_coherence,rho_all_x_y_ort_x,'b.')
@@ -193,7 +232,7 @@ xlabel('coherence')
 ylabel('correlation coeff')
 
 h_s(3)=subplot(2,4,3);
-plot(range_coherence,rho_all_plain,'k.')
+plot(range_coherence,rho_all_x_y_plain,'k.')
 hold on
 plot(range_coherence,rho_all_x2_x_ort_y,'go')
 plot(range_coherence,rho_all_x2_y_ort_x,'g.')
@@ -202,7 +241,7 @@ xlabel('coherence')
 ylabel('correlation coeff')
 
 h_s(4)=subplot(2,4,4);
-plot(range_coherence,rho_all_plain,'k.')
+plot(range_coherence,rho_all_x_y_plain,'k.')
 hold on
 plot(range_coherence,rho_all_y_x_ort_y,'ro')
 plot(range_coherence,rho_all_y_y_ort_x,'r.')
@@ -211,36 +250,36 @@ xlabel('coherence')
 ylabel('correlation coeff')
 
 h_s(5)=subplot(2,4,5);
-plot(rho_all_plain,rho_all_plain,'k.')
+plot(rho_all_x_y_plain,rho_all_x_y_plain,'k.')
 hold on
-plot(rho_all_plain,rho_all_ort,'m.')
+plot(rho_all_x_y_plain,rho_all_ort,'m.')
 legend({'plain','(x ort y , y ort x)'},'interpreter','none')
 xlabel('correlation')
 ylabel('correlation coeff')
 
 h_s(6)=subplot(2,4,6);
-plot(rho_all_plain,rho_all_plain,'k.')
+plot(rho_all_x_y_plain,rho_all_x_y_plain,'k.')
 hold on
-plot(rho_all_plain,rho_all_x_x_ort_y,'bo')
-plot(rho_all_plain,rho_all_x_y_ort_x,'b.')
+plot(rho_all_x_y_plain,rho_all_x_x_ort_y,'bo')
+plot(rho_all_x_y_plain,rho_all_x_y_ort_x,'b.')
 legend({'plain','(x , x_ort_y)','(x_, y_ort_x)'},'interpreter','none')
 xlabel('correlation')
 ylabel('correlation coeff')
 
 h_s(7)=subplot(2,4,7);
-plot(rho_all_plain,rho_all_plain,'k.')
+plot(rho_all_x_y_plain,rho_all_x_y_plain,'k.')
 hold on
-plot(rho_all_plain,rho_all_x2_x_ort_y,'go')
-plot(rho_all_plain,rho_all_x2_y_ort_x,'g.')
+plot(rho_all_x_y_plain,rho_all_x2_x_ort_y,'go')
+plot(rho_all_x_y_plain,rho_all_x2_y_ort_x,'g.')
 legend({'plain','(x2 , x_ort_y)','(x2 , y_ort_x)'},'interpreter','none')
 xlabel('correlation')
 ylabel('correlation coeff')
 
 h_s(8)=subplot(2,4,8);
-plot(rho_all_plain,rho_all_plain,'k.')
+plot(rho_all_x_y_plain,rho_all_x_y_plain,'k.')
 hold on
-plot(rho_all_plain,rho_all_y_x_ort_y,'ro')
-plot(rho_all_plain,rho_all_y_y_ort_x,'r.')
+plot(rho_all_x_y_plain,rho_all_y_x_ort_y,'ro')
+plot(rho_all_x_y_plain,rho_all_y_y_ort_x,'r.')
 legend({'plain','(y , x_ort_y)','(y , y_ort_x)'},'interpreter','none')
 xlabel('correlation')
 ylabel('correlation coeff')
@@ -254,7 +293,7 @@ x_phases=angle(x).*180/pi;
 x_2_phases=angle(x_2).*180/pi;
 y_phases=angle(y).*180/pi;
 y_ort_x_phases=angle(y_ort_x).*180/pi;
-x_ort_y_phases=angle(x_ort_y).*180/pi; 
+x_ort_y_phases=angle(x_ort_y).*180/pi;
 
 figure
 subplot(1,3,1)
@@ -311,7 +350,7 @@ h_compass=zeros(1,length(samples));
 for curr_sample=samples
     h_compass(curr_sample)=subplot(2,5,curr_sample);
     for curr_cohere_inx=1:1:length(range_coherence)
-        compass(real(y(curr_cohere_inx,curr_sample)),imag(y(curr_cohere_inx,curr_sample)),'r')       
+        compass(real(y(curr_cohere_inx,curr_sample)),imag(y(curr_cohere_inx,curr_sample)),'r')
         hold on
         compass(real(y_ort_x(curr_cohere_inx,curr_sample)),imag(y_ort_x(curr_cohere_inx,curr_sample)),'k')
         compass(real(x_ort_y(curr_cohere_inx,curr_sample)),imag(x_ort_y(curr_cohere_inx,curr_sample)),'m')
